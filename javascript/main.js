c06f5b5befa2b901187fc47cab3fb47a239fae07
jasonstreet <jason.street11@gmail.com>
2019-04-02 14:37:59 +0100

    beautified

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,6 @@
 // --THE FOLLOWING CODE IS ONLY FOR APOD - ASTRONOMY PICTURE OF THE DAY -- //
 
 // My Global Variables
-
 var currentAPOD; // Astronomy Picture of the day - current day
 var myDataAPOD; // All data requested by API - APOD only.
 
@@ -17,25 +16,25 @@ var getAPOD = document.getElementById("showAPOD").addEventListener("click", load
 function loadMyDataAPOD() {
 
     var myRequest = new XMLHttpRequest();
-    
+
     myRequest.open("GET", "https://api.nasa.gov/planetary/apod?api_key=h27SgJssM7Qh0G0xPFPBky4D8HFyfsL0yy9KSsBq", true);
-    
-    myRequest.onload = function () {
-        
+
+    myRequest.onload = function() {
+
         if (myRequest.readyState === 4 && myRequest.status === 200) {
-            
+
             var myDataAPOD = JSON.parse(myRequest.responseText);
-            
+
             document.getElementById("explanationAPOD").innerHTML = myDataAPOD.explanation;
             document.getElementById("titleAPOD").innerHTML = myDataAPOD.title;
             document.getElementById("copyrightAPOD").innerHTML = "©" + myDataAPOD.copyright;
             document.getElementById("currentAPOD").innerHTML = '<img src="' + myDataAPOD.url + '"width="auto" height="auto" alt="Image of the day">';
             //document.getElementById("myImages").src= myData.url;  //Alternative method of dynamic images. Requires proper IMG tagging in paired HTML Doc.
         }
-    
+
     };
     myRequest.send();
-    
+
 }
 
 
@@ -63,41 +62,41 @@ var getISS = document.getElementById("showISS").addEventListener("click", loadMy
 
 
 function loadMyDataISS() {
-    
+
 
 
     var myRequest = new XMLHttpRequest();
-    
+
     myRequest.open("GET", "https://api.wheretheiss.at/v1/satellites/25544", true);
-    
-    myRequest.onload = function () {
-        
+
+    myRequest.onload = function() {
+
         if (myRequest.readyState === 4 && myRequest.status === 200) {
-            
+
             var myDataISS = JSON.parse(myRequest.responseText);
             console.log(myDataISS); // - Test if the API is properly linked on click.
-            
+
             document.getElementById("altitudeISS").innerHTML = myDataISS.altitude;
             document.getElementById("longitudeISS").innerHTML = myDataISS.longitude;
             document.getElementById("latitudeISS").innerHTML = myDataISS.latitude;
             document.getElementById("currentVelocityISS").innerHTML = myDataISS.velocity;
             document.getElementById('map').style.display = 'block';
-            var mapProp= {
-              center:new google.maps.LatLng(myDataISS.latitude,myDataISS.longitude),
-              zoom: 4,
+            var mapProp = {
+                center: new google.maps.LatLng(myDataISS.latitude, myDataISS.longitude),
+                zoom: 4,
             };
-            var map = new google.maps.Map(document.getElementById("map"),mapProp);  
+            var map = new google.maps.Map(document.getElementById("map"), mapProp);
             var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(myDataISS.latitude, myDataISS.longitude),
-            map: map,
-            });  
-                       
-            
+                position: new google.maps.LatLng(myDataISS.latitude, myDataISS.longitude),
+                map: map,
+            });
+
+
         }
-    
+
     };
     myRequest.send();
-    
+
 }
 
 
@@ -117,103 +116,100 @@ var stars = document.querySelectorAll('.animated-card .animated .stars div'),
     offsetY,
     isMoving = false,
     addBounceAnimation = function(element, time) {
-      element.style.transition = "transform linear " + time + "ms";
-      element.style.transform = "scaleY(1)";
-      setTimeout(function() {
-        element.style.transition = "transform linear " + time/2 + "ms";
-        customTimeout(element, time, 0.1, 5);
-      }, time/2);
+        element.style.transition = "transform linear " + time + "ms";
+        element.style.transform = "scaleY(1)";
+        setTimeout(function() {
+            element.style.transition = "transform linear " + time / 2 + "ms";
+            customTimeout(element, time, 0.1, 5);
+        }, time / 2);
     },
     customTimeout = function(element, time, value, repeat) {
-      value = value/2;
-      setTimeout(function() {
-        if(repeat > 0) {
-          element.style.transform = "scaleY(" + ( 1 + value) + ")";
-          customTimeout(element, time, value, repeat - 1);
-        } else {
-          element.style.transition = "none";
-        }
-      }, time/2)
+        value = value / 2;
+        setTimeout(function() {
+            if (repeat > 0) {
+                element.style.transform = "scaleY(" + (1 + value) + ")";
+                customTimeout(element, time, value, repeat - 1);
+            } else {
+                element.style.transition = "none";
+            }
+        }, time / 2)
     },
-    
+
     // Round values for star animations
     changeStar = function(element, index, array) {
-      var posX = Math.floor((Math.random() * 100) + 1),
-          posY = Math.floor((Math.random() * 100) + 1),
-          scale = Math.random(),
-          animationTime = Math.floor((Math.random() * (5000 - 10000)) + 10000)
-      ;
-      element.style.left = posX + "%";
-      element.style.top = posY + "%";
-      element.style.transform = "scale("+ scale + ")";
-      element.style.animationDuration = animationTime + "ms";
+        var posX = Math.floor((Math.random() * 100) + 1),
+            posY = Math.floor((Math.random() * 100) + 1),
+            scale = Math.random(),
+            animationTime = Math.floor((Math.random() * (5000 - 10000)) + 10000);
+        element.style.left = posX + "%";
+        element.style.top = posY + "%";
+        element.style.transform = "scale(" + scale + ")";
+        element.style.animationDuration = animationTime + "ms";
     },
-    
+
     // Round values for planet animations
     changePlanet = function(element, index, array) {
-          animationTime = Math.floor((Math.random() * (20000 - 30000)) + 30000)
-      ;
-      element.style.animationDuration = animationTime + "ms";
+        animationTime = Math.floor((Math.random() * (20000 - 30000)) + 30000);
+        element.style.animationDuration = animationTime + "ms";
     },
-    
+
     touchStart = function(e) {
-      var touch = e.changedTouches[0];
-      
-      startX = touch.pageX;
-      startY = touch.pageY;
-      
-      // Add Rocket vibe
-      rocket.classList.add('vibe');
-      
+        var touch = e.changedTouches[0];
+
+        startX = touch.pageX;
+        startY = touch.pageY;
+
+        // Add Rocket vibe
+        rocket.classList.add('vibe');
+
     },
     touchMove = function(e) {
-      e.preventDefault();
-      
-      var touch = e.changedTouches[0];
-      
-      offsetX = touch.pageX - startX;
-      offsetY = touch.pageY - startY;
-      
-      // Direction = Vertical?
-      if(offsetX < offsetY) {
-        background.style.transformOrigin = "top";
-        
-        // Transform if inside limits
-        if((1 + offsetY/1000) <= 1.2 && (1 + offsetY/1000) >= 1) {
-          background.style.transform = "scaleY(" + (1 + offsetY/1000) + ") translateY(0)";
-          isMoving = true;
+        e.preventDefault();
+
+        var touch = e.changedTouches[0];
+
+        offsetX = touch.pageX - startX;
+        offsetY = touch.pageY - startY;
+
+        // Direction = Vertical?
+        if (offsetX < offsetY) {
+            background.style.transformOrigin = "top";
+
+            // Transform if inside limits
+            if ((1 + offsetY / 1000) <= 1.2 && (1 + offsetY / 1000) >= 1) {
+                background.style.transform = "scaleY(" + (1 + offsetY / 1000) + ") translateY(0)";
+                isMoving = true;
+            }
+
+            if (offsetY / 1000 <= 0) {
+                isMoving = false;
+            }
         }
-        
-        if(offsetY/1000 <= 0) {
-          isMoving = false;
-        }
-      }
-      
+
     },
     touchEnd = function(e) {
-      
-      addBounceAnimation(background,200);
-      background.classList.add('bounce');
-      
-      // Add fly animation
-      if(isMoving === true) {
-        rocket.classList.add('fly');
-        setTimeout(function() {
-          isMoving = false;
-          if(isMoving === false) {
-            rocket.classList.remove('vibe');
-            rocket.classList.remove('fly');
-          }
-        }, 7000);
-        
-        // Remove Rocket vibe
-        if(isMoving === false) {
-          rocket.classList.remove('vibe');
-          rocket.classList.remove('fly');
+
+        addBounceAnimation(background, 200);
+        background.classList.add('bounce');
+
+        // Add fly animation
+        if (isMoving === true) {
+            rocket.classList.add('fly');
+            setTimeout(function() {
+                isMoving = false;
+                if (isMoving === false) {
+                    rocket.classList.remove('vibe');
+                    rocket.classList.remove('fly');
+                }
+            }, 7000);
+
+            // Remove Rocket vibe
+            if (isMoving === false) {
+                rocket.classList.remove('vibe');
+                rocket.classList.remove('fly');
+            }
         }
-      }
-    }
-;
+    };
 
 stars.forEach(changeStar);
 planets.forEach(changePlanet);
